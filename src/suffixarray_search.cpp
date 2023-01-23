4a5d4dcd0a1978b8d105e126b00f3109a04d9860
Katherina <lunches.global-0p@icloud.com>
2023-01-23 08:23:27 +0100

    xx

--- src/suffixarray_search.cpp
+++ src/suffixarray_search.cpp
@@ -53,7 +53,7 @@ void find(sauchar_t const* query, const sauchar_t* text, saidx_t *SA, saidx_t m,
         }
 
         Lp = right;
-        if (indexx == 0) left = Lp - 1;
+        if (index == 0) left = Lp - 1;
         else left = Lp;
         right = Rp;
 
